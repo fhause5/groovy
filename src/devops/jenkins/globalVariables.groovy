9c672c3d1705a3f77810a1c77268b36287d7cf54
Ihor <ihor.k@twsgo.com>
2020-08-04 13:13:52 +0300

    globalVariables

--- src/devops/jenkins/globalVariables.groovy
+++ src/devops/jenkins/globalVariables.groovy
@@ -4,10 +4,23 @@ package devops.jenkins.globalVariables
 
 
 class globalVariables {
-    DB_SID='TRDSRV2'
-    DB_ENDPOINT='real1.cx7phmaycyde.eu-west-1.rds.amazonaws.com',
-    DB_PORT='1521'
-    DB_PROTOCOL='tcp'
+    void DB_SID(){
+        DB_SID='TRDSRV2'
+        return DB_SID
+    }
+    void DB_ENDPOINT() {
+        DB_ENDPOINT='real1.cx7phmaycyde.eu-west-1.rds.amazonaws.com'
+        return DB_ENDPOINT
+    }
+    void DB_PORT() {
+        DB_PORT='1521'
+        return DB_PORT
+    }
+    void DB_PROTOCOL() {
+        DB_PROTOCOL='tcp'
+        return DB_PROTOCOL
+    }
+    
 }
 
 
